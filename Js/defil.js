3c443043cbab2faccd5a73fcf19909fa04e05e21
tom_ballester <137886466+tom123456789123456@users.noreply.github.com>
2023-10-20 00:20:27 +0200

    Update responsive

--- Js/defil.js
+++ Js/defil.js
@@ -1,16 +1,31 @@
 document.querySelector('a[href="#Contact"]').addEventListener('click', function (e) {
-    e.preventDefault(); // Empêche le comportement de lien par défaut
+    e.preventDefault(); // Empecher le comportement de lien normal
 
     const target = document.querySelector('#Contact');
     const offset = target.getBoundingClientRect().top + window.scrollY;
-    const duration = 1000; // Durée de l'animation en millisecondes (1 seconde)
+    const duration = 1000; // Durée de l'animation en millisecondes
 
     window.scrollTo({
         top: offset,
-        behavior: 'smooth', // Active l'animation en douceur
+        behavior: 'smooth', // Type d'annimation
         duration: duration
     });
 });
+
+
+document.querySelector('.menu-incolumn a[href="#Contact"]').addEventListener('click', function (e) {
+    e.preventDefault(); 
+
+    const target = document.querySelector('#Contact');
+    const offset = target.getBoundingClientRect().top + window.scrollY;
+    const duration = 1000; 
+    window.scrollTo({
+        top: offset,
+        behavior: 'smooth', 
+        duration: duration
+    });
+});
+
 
 document.querySelector('a[href="#Top"]').addEventListener('click', function (e) {
     e.preventDefault();
@@ -25,6 +40,7 @@ document.querySelector('a[href="#Top"]').addEventListener('click', function (e) 
         duration: duration
     });
 });
+
 
 const boutonTop = document.querySelector('.bouton-top');
 
---
+++ Js/phoneMenu.js
@@ -0,0 +1,37 @@
+var sidenav = document.getElementById("sideNav");
+var openBtn = document.getElementById("openBtn");
+var isNavOpen = false; // Variable pour suivre l'état du menu
+
+// Ouvrir ou fermer le menu lors du clic sur le bouton
+openBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (isNavOpen) {
+        closeNav();
+    } else {
+        openNav();
+    }
+});
+
+// Fermer le menu lors du clic en dehors du menu
+document.addEventListener("click", function (event) {
+    if (isNavOpen && !sidenav.contains(event.target) && !openBtn.contains(event.target)) {
+        closeNav();
+    }
+});
+
+// Fermer le menu lors du défilement de la page
+window.addEventListener("scroll", function () {
+    if (isNavOpen) {
+        closeNav();
+    }
+});
+
+function openNav() {
+    sidenav.classList.add("active");
+    isNavOpen = true;
+}
+
+function closeNav() {
+    sidenav.classList.remove("active");
+    isNavOpen = false;
+}
