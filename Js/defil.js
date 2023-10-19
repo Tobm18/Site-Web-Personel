ace3388dde46f31f016acb70aff3c1565cae7b7c
tom_ballester <137886466+tom123456789123456@users.noreply.github.com>
2023-10-19 16:13:54 +0200

    add top button + responsive

--- Js/defil.js
+++ Js/defil.js
@@ -11,3 +11,27 @@ document.querySelector('a[href="#Contact"]').addEventListener('click', function 
         duration: duration
     });
 });
+
+document.querySelector('a[href="#Top"]').addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector('#Top');
+    const offset = target.getBoundingClientRect().top + window.scrollY;
+    const duration = 1000; 
+
+    window.scrollTo({
+        top: offset,
+        behavior: 'smooth',
+        duration: duration
+    });
+});
+
+const boutonTop = document.querySelector('.bouton-top');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY === 0) {
+        boutonTop.style.display = 'none'; // Cacher la fleche
+    } else {
+        boutonTop.style.display = 'block'; // Afficher la fleche
+    }
+});
