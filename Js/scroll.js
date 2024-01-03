3a6fdc87d6a1c14f9bbde456818f3f1adf501e71
tom_ballester <137886466+tom123456789123456@users.noreply.github.com>
2024-01-03 20:27:18 +0100

    Fix scroll behavior + add timeline + add 2 new card

---
+++ Js/scroll.js
@@ -0,0 +1,182 @@
+gsap.registerPlugin(ScrollToPlugin);
+
+// Accueil
+
+document.querySelector('a[href="#Accueil"]').addEventListener('click', function (e) {
+    e.preventDefault(); // Empecher le comportement de lien normal
+
+    const target = document.querySelector('#Accueil');
+    const offset = target.getBoundingClientRect().top + window.scrollY;
+    const duration = 1; // Durée de l'animation 
+
+    gsap.to(window, {
+        duration: duration,
+        scrollTo: {
+            y: offset,
+            autoKill: false
+        },
+        ease: "power4.inOut"
+    });
+});
+
+
+document.querySelector('.menu-incolumn a[href="#Accueil"]').addEventListener('click', function (e) {
+    e.preventDefault(); 
+
+    const target = document.querySelector('#Accueil');
+    const offset = target.getBoundingClientRect().top + window.scrollY;
+    const duration = 1; 
+    gsap.to(window, {
+        duration: duration,
+        scrollTo: {
+            y: offset,
+            autoKill: false
+        },
+        ease: "power4.inOut"
+    });
+});
+
+
+// Contact
+
+document.querySelector('a[href="#Contact"]').addEventListener('click', function (e) {
+    e.preventDefault(); 
+
+    const target = document.querySelector('#Contact');
+    const offset = target.getBoundingClientRect().top + window.scrollY;
+    const duration = 1; 
+
+    window.scrollTo({
+        top: offset,
+        duration: duration,
+        behavior: 'smooth'
+    });
+});
+
+
+document.querySelector('.menu-incolumn a[href="#Contact"]').addEventListener('click', function (e) {
+    e.preventDefault(); 
+
+    const target = document.querySelector('#Contact');
+    const offset = target.getBoundingClientRect().top + window.scrollY;
+    const duration = 1; 
+    
+    window.scrollTo({
+        top: offset,
+        duration: duration,
+        behavior: 'smooth'
+    });
+});
+
+// Réalisation
+
+document.querySelector('a[href="#realisation"]').addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector('#realisation');
+    const offset = target.getBoundingClientRect().top + window.scrollY;
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+    const offsetWithVh = offset - (0.1 * vh); // 10vh
+    const duration = 1;
+
+    gsap.to(window, {
+        duration: duration,
+        scrollTo: {
+            y: offsetWithVh,
+            autoKill: false
+        },
+        ease: "power4.inOut"
+    });
+});
+
+document.querySelector('.menu-incolumn a[href="#realisation"]').addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector('#realisation');
+    const offset = target.getBoundingClientRect().top + window.scrollY;
+    const duration = 1;
+
+    gsap.to(window, {
+        duration: duration,
+        scrollTo: {
+            y: offset,
+            autoKill: false
+        },
+        ease: "power4.inOut"
+    });
+});
+
+// Timeline
+
+document.querySelector('a[href="#About"]').addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector('#About');
+    const offset = target.getBoundingClientRect().top + window.scrollY;
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+    const offsetWithVh = offset - (0.09 * vh); // 10vh
+    const duration = 1;
+
+    gsap.to(window, {
+        duration: duration,
+        scrollTo: {
+            y: offsetWithVh,
+            autoKill: false
+        },
+        ease: "power4.inOut"
+    });
+});
+
+document.querySelector('.menu-incolumn a[href="#About"]').addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector('#About');
+    const offset = target.getBoundingClientRect().top + window.scrollY;
+    const duration = 1;
+
+    gsap.to(window, {
+        duration: duration,
+        scrollTo: {
+            y: offset,
+            autoKill: false
+        },
+        ease: "power4.inOut"
+    });
+});
+
+// Bouton Top
+
+document.querySelector('a[href="#Top"]').addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector('#Top');
+    const offset = target.getBoundingClientRect().top + window.scrollY;
+    const duration = 1000; 
+
+    window.scrollTo({
+        top: offset,
+        duration: duration,
+        behavior: 'smooth'
+    });
+});
+
+
+const boutonTop = document.querySelector('.bouton-top');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY === 0) {
+        boutonTop.style.display = 'none'; // Cacher la fleche
+    } else {
+        boutonTop.style.display = 'block'; // Afficher la fleche
+    }
+});
+
+const topMenu = document.querySelector('.menu-inline');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY === 0) {
+        topMenu.style.background = 'rgba(0, 0, 0, 0.25)'; 
+    } else {
+        topMenu.style.background = 'rgb(34 34 34)';
+    }
+});
